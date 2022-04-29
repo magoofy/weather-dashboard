7d015528d0d8c03b94f728716148581fe2e70224
sakau <sakau@usc.edu>
2022-04-28 22:42:28 -0700

    finished positioning and display issues, created function to display API information

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,46 @@
-var searchedCity = document.querySelector("#city").value;
+var searchedCity = document.querySelector("#city");
 var submitBtn = document.querySelector("#submit");
 var searchHistory = document.querySelector("#search-history");
 var dayForecast = document.querySelector("day-forecast");
 var weekForecast = document.querySelector("#week-forecast");
+var currentCity = document.querySelector("#current-city");
 var dayTemp = document.querySelector("#temp");
 var dayWind = document.querySelector("#wind");
 var dayHumidity = document.querySelector("#humidity");
 var uvIndex = document.querySelector("#uv-index");
 var weekForecastContainer = document.querySelector("#week-forecast");
 
+var formSubmitHandler = function(event) {
+    event.preventDefault()
+
+    var city = searchedCity.value.trim();
+
+    if (city) {
+        getWeather(city);
+
+        searchedCity.value = "";
+        weekForecast.textContent = "";
+        dayTemp.textContent = "";
+        dayWind.textContent = "";
+        dayHumidity.textContent = "";
+        uvIndex.textContent = "";
+        
+    } else {
+        alert("Please enter a city")
+
+    }
+}
+
 var getWeather = function() {
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=e1fdfa2386872dd651201114b0cdeacd"    
     fetch(apiUrl).then(function(response){
         if(response.ok) {
-            console.log(response);
             response.json().then(function(data){
-                console.log(data);
+                //EVENTUALLY INPUT CITY AS SECOND PARAMETER
+                displayWeather(data);
             })
         } else {
-            alert("Not a valid entry");
+            alert("An Error Occured");
         }
     })
     .catch(function(error){
@@ -26,4 +48,15 @@ var getWeather = function() {
     })
 }
 
-getWeather();
+var displayWeather = function(weather) {
+    console.log(weather)
+    //currentCity.textContent = 
+    dayTemp.textContent = weather.current.temp + " F";
+    dayWind.textContent = weather.current.wind_speed + " MPH";
+    dayHumidity.textContent = weather.current.humidity + "%";
+    uvIndex.textContent = weather.current.uvi;
+
+
+}
+
+submitBtn.addEventListener('click', formSubmitHandler);
