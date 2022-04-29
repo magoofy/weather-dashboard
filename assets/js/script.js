352adaadfc6b754cad765f25179c758edddafcee
sakau <sakau@usc.edu>
2022-04-29 14:33:52 -0700

    added js to create  5-day forecast, added css styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,7 +32,7 @@ var formSubmitHandler = function(event) {
 }
 
 var getWeather = function() {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=e1fdfa2386872dd651201114b0cdeacd"    
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=34.0522&lon=-118.2437&appid=e1fdfa2386872dd651201114b0cdeacd"    
     fetch(apiUrl).then(function(response){
         if(response.ok) {
             response.json().then(function(data){
@@ -49,14 +49,43 @@ var getWeather = function() {
 }
 
 var displayWeather = function(weather) {
+    
+    // DAY FORECAST
     console.log(weather)
     //currentCity.textContent = 
     dayTemp.textContent = weather.current.temp + " F";
     dayWind.textContent = weather.current.wind_speed + " MPH";
     dayHumidity.textContent = weather.current.humidity + "%";
-    uvIndex.textContent = weather.current.uvi;
+    uvIndex.textContent = weather.current.uvi ;
+    if (weather.current.uvi <= 2) {
+        uvIndex.className = "good";
+    } else if (2 < weather.current.uvi <= 7) {
+        uvIndex.className = "average";
+    } else {
+        uvIndex.className = "bad";
+    }
 
+    // 5-DAY FORECAST
+    for (var i = 0; i < 5; i++) {
+    var weeklyCard = document.createElement("div")
+    weeklyCard.className = "weekly-card";
+    var weeklyIcon = document.createElement("img")
+    var weeklyTemp = document.createElement("div")
+    var weeklyHumidity = document.createElement("div")
+    var weeklyWind = document.createElement("div")
+    var iconCode = weather.daily[i].weather[0].icon
+    
+    weeklyIcon.src = 'http://openweathermap.org/img/wn/' + iconCode + '@2x.png';
+    weeklyTemp.textContent = 'Temp: ' + weather.daily[i].temp.day + ' F';
+    weeklyHumidity.textContent = 'Humidity: ' + weather.daily[i].humidity + '%';
+    weeklyWind.textContent = 'Wind: ' + weather.daily[i].wind_speed + ' MPH';
 
+    weekForecast.appendChild(weeklyCard);
+    weeklyCard.appendChild(weeklyIcon);
+    weeklyCard.appendChild(weeklyTemp);
+    weeklyCard.appendChild(weeklyHumidity);
+    weeklyCard.appendChild(weeklyWind);
+    }
 }
 
 submitBtn.addEventListener('click', formSubmitHandler);
